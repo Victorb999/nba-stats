ce7ab28fa9ddfec7420e262c34b014c704cd7197
Victor Araújo <victor.b_@hotmail.com>
2021-09-10 23:58:06 -0300

    update player page

--- src/pages/player/[slug].tsx
+++ src/pages/player/[slug].tsx
@@ -1,22 +1,47 @@
 import { GetServerSideProps } from "next";
-import { apiBio } from "../../services/api";
+import { api } from "../../services/api";
 
 import ImageWithFallback from "../../utils/ImageWithCallBack";
+import { Standard } from "../../services/typesPlayer";
 
-function Player({ player }) {
+import Image from "next/image";
+
+function Player({ player,stats }) {
   // Render data...
   return (
     <div>
-      <h1>{player.display_name}</h1>
+      {/* {JSON.stringify(player)} */}
       <ImageWithFallback
         width={200}
         height={200}
-        src={`https://cdn.nba.com/headshots/nba/latest/1040x760/${player.id}.png`}
-        alt={player.id}
-        objectFit="cover"
+        src={`https://cdn.nba.com/headshots/nba/latest/1040x760/${player.personId}.png`}
+        alt={player.personId}
         fallbackSrc={`https://cdn.nba.com/headshots/nba/latest/1040x760/logoman.png`}
       />
-      <div dangerouslySetInnerHTML={{ __html: player.professional }} />
+      <div>
+        <h1>#{`${player.jersey}  ${player.firstName}  ${player.lastName}`}</h1>
+        <Image
+          width={50}
+          height={50}
+          src={`https://cdn.nba.com/logos/nba/${player.teamId}/global/L/logo.svg`}
+          alt={`Team`}
+          objectFit="cover"
+        />
+        <h2>{player.teamSitesOnly.posFull}</h2>
+        <h3>{`${player.heightMeters}m ${player.weightKilograms}Kg`}</h3>
+        <h3>{player.dateOfBirthUTC}</h3>{/*calc years*/}
+      </div>
+      <div>
+        <p>Pick #{player.draft.pickNum} </p>
+        <p>Country: {player.country} </p>
+        <p>
+          Round: {player.draft.roundNum}/{player.draft.seasonYear}
+        </p>
+        <p>College: {player.collegeName} </p>
+      </div>
+      <div>
+        {JSON.stringify(stats)}
+      </div>
     </div>
   );
 }
@@ -25,14 +50,21 @@ function Player({ player }) {
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   //const data = { player: params.slug };
 
-  const player = await apiBio
-    .get(`/player_${params.slug}.json`)
-    .then((player) => {
-      return player.data.Bio;
-    });
+  const players = await api.get("/players.json").then((player) => {
+    return player.data.league.standard;
+  });
+
+  let player = players.filter((player: Standard) => {
+    return player.personId === params.slug;
+  });
+  player = player[0];
+
+  const stats = await api.get(`/players/${params.slug}_profile.json`).then((stats) => {
+    return stats.data.league.standard.stats.latest;
+  });
 
   // Pass data to the page via props
-  return { props: { player } };
+  return { props: { player,stats } };
 };
 
 export default Player;
--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,3 @@ const year = date.getFullYear();
 export const api = axios.create({
   baseURL: `https://data.nba.net/10s/prod/v1/${year}/`
 });
-
-export const apiBio = axios.create({
-  baseURL: `https://data.nba.net/json/bios/`
-});
