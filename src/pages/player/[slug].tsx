0734853b856df81cbe9f5de2e6af6b1308d4e703
Victor Araújo <48557502+Victorb999@users.noreply.github.com>
2021-09-13 11:10:04 -0300

    changes player page

--- src/pages/player/[slug].tsx
+++ src/pages/player/[slug].tsx
@@ -4,6 +4,7 @@ import { api } from "../../services/api";
 import ImageWithFallback from "../../utils/ImageWithCallBack";
 import { Standard } from "../../services/typesPlayer";
 
+import Head from "next/head";
 import Image from "next/image";
 import { useTeam } from "../../contexts/TeamContext";
 
@@ -20,6 +21,9 @@ function Player({ player, stats }) {
 
   return (
     <div>
+      <Head>
+        <title>{`${player.firstName}  ${player.lastName}`} | NBA stats</title>
+      </Head>
       <div className={styles.playerContainer}>
         {/* {JSON.stringify(player)} */}
         <div className={styles.playerPhoto}>
@@ -97,56 +101,55 @@ function Player({ player, stats }) {
         </div>
       </div>
       <div className={styles.playerSubContainer}>
-
-      
-      <div className={styles.playerStats}>
-        <div className={styles.playerTitle + " " + teamName}>
-          <h1>Career Stats</h1>
-        </div>
-        <div className={styles.playerStatsContainer}>
-          <div>
-            <h2>ppg: {stats.ppg}</h2>
-            <h2>rpg: {stats.rpg}</h2>
-            <h2>apg: {stats.apg}</h2>
-            <h2>mpg: {stats.mpg}</h2>
-            <h2>spg: {stats.spg}</h2>
-            <h2>bpg: {stats.bpg}</h2>
-            <h2>tpp: {stats.tpp}</h2>
+        <div className={styles.playerStats}>
+          <div className={styles.playerTitle + " " + teamName}>
+            <h1>Career Stats</h1>
           </div>
-          <div>
-            <h2>ftp: {stats.ftp}</h2>
-            <h2>fgp: {stats.fgp}</h2>
-            <h2>assists: {stats.assists}</h2>
-            <h2>blocks: {stats.blocks}</h2>
-            <h2>steals: {stats.steals}</h2>
-            <h2>turnovers: {stats.turnovers}</h2>
-          </div>
-          <div>
-            <h2>offReb: {stats.offReb}</h2>
-            <h2>defReb: {stats.defReb}</h2>
-            <h2>totReb: {stats.totReb}</h2>
-            <h2>fgm: {stats.fgm}</h2>
-            <h2>fga: {stats.fga}</h2>
-            <h2>tpm: {stats.tpm}</h2>
-          </div>
-          <div>
-            <h2>tpa: {stats.tpa}</h2>
-            <h2>ftm: {stats.ftm}</h2>
-            <h2>fta: {stats.fta}</h2>
-            <h2>pFouls: {stats.pFouls}</h2>
-            <h2>points: {stats.points}</h2>
-            <h2>games played: {stats.gamesPlayed}</h2>
-          </div>
-          <div>
-            <h2>games started: {stats.gamesStarted}</h2>
-            <h2>plus minus: {stats.plusMinus}</h2>
-            <h2>min: {stats.min}</h2>
-            <h2>dd2: {stats.dd2}</h2>
-            <h2>td3: {stats.td3}</h2>
+          <div className={styles.playerStatsContainer}>
+            <div>
+              <h2>ppg: {stats.ppg}</h2>
+              <h2>rpg: {stats.rpg}</h2>
+              <h2>apg: {stats.apg}</h2>
+              <h2>mpg: {stats.mpg}</h2>
+              <h2>spg: {stats.spg}</h2>
+              <h2>bpg: {stats.bpg}</h2>
+              <h2>tpp: {stats.tpp}</h2>
+            </div>
+            <div>
+              <h2>ftp: {stats.ftp}</h2>
+              <h2>fgp: {stats.fgp}</h2>
+              <h2>assists: {stats.assists}</h2>
+              <h2>blocks: {stats.blocks}</h2>
+              <h2>steals: {stats.steals}</h2>
+              <h2>turnovers: {stats.turnovers}</h2>
+            </div>
+            <div>
+              <h2>offReb: {stats.offReb}</h2>
+              <h2>defReb: {stats.defReb}</h2>
+              <h2>totReb: {stats.totReb}</h2>
+              <h2>fgm: {stats.fgm}</h2>
+              <h2>fga: {stats.fga}</h2>
+              <h2>tpm: {stats.tpm}</h2>
+            </div>
+            <div>
+              <h2>tpa: {stats.tpa}</h2>
+              <h2>ftm: {stats.ftm}</h2>
+              <h2>fta: {stats.fta}</h2>
+              <h2>pFouls: {stats.pFouls}</h2>
+              <h2>points: {stats.points}</h2>
+              <h2>games played: {stats.gamesPlayed}</h2>
+            </div>
+            <div>
+              <h2>games started: {stats.gamesStarted}</h2>
+              <h2>plus minus: {stats.plusMinus}</h2>
+              <h2>min: {stats.min}</h2>
+              <h2>dd2: {stats.dd2}</h2>
+              <h2>td3: {stats.td3}</h2>
+            </div>
           </div>
         </div>
       </div>
-    </div></div>
+    </div>
   );
 }
 
