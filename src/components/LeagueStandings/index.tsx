b44f9133c6df18da1e9535ecb967095be814bdfd
Victor Araújo <victor.b_@hotmail.com>
2021-09-21 23:02:08 -0300

    building standings in home

--- src/components/LeagueStandings/index.tsx
+++ src/components/LeagueStandings/index.tsx
@@ -11,12 +11,8 @@ export default function LeagueStandings() {
   const [standings, setStandings] = useState<StandardStandings>(
     {} as StandardStandings
   );
-  const [west, setWest] = useState<West[]>(
-    [] as West[]
-  );
-  const [east, setEast] = useState<East[]>(
-    [] as East[]
-  );
+  const [west, setWest] = useState<West[]>([] as West[]);
+  const [east, setEast] = useState<East[]>([] as East[]);
   const setCallback = useCallback(async () => {
     const standings = await getStandings();
     setStandings(standings);
@@ -24,82 +20,89 @@ export default function LeagueStandings() {
     setEast(standings.conference.east);
   }, [setStandings]);
 
-
   useEffect(() => {
-     setCallback();
+    setCallback();
   }, [setCallback]);
 
   return (
-    <div className="welcomeContainer">
+    <div className={styles.allStandings}>
       <h1>League Standings</h1>
       <h2>{standings.seasonYear}</h2>
-      
 
-     <div className={styles.standingsContainer}>
+      <div className={styles.standingsContainer}>
         <div className={styles.standingsConference}>
-          <h3>East</h3>
+          <div className={styles.conferenceTitle}>
+            <h3>East</h3>
+          </div>
           {east.map((team) => {
             return (
-              <div key={team.teamId} className={
-                parseInt(team.confRank) <= 8 ?
-                styles.standingsTeam + " " + styles.playoffs:
-                styles.standingsTeam
-                }>
+              <div
+                key={team.teamId}
+                className={
+                  parseInt(team.confRank) <= 8
+                    ? styles.standingsTeam + " " + styles.playoffs
+                    : styles.standingsTeam
+                }
+              >
                 <span>{team.confRank}</span>
-                <span>
                 <Image
-                    width={40}
-                    height={40}
-                    src={`https://cdn.nba.com/logos/nba/${team.teamId}/global/D/logo.svg`}
-                    alt={team.teamSitesOnly.teamNickname}
-                    objectFit="cover"
-                  />
-                  {team.teamSitesOnly.teamName +
-                    " " +
-                    team.teamSitesOnly.teamNickname}
-                </span>
-                <span>
-                  {team.win +
-                    "-" +
-                    team.loss}
-                </span>
+                  width={40}
+                  height={40}
+                  src={`https://cdn.nba.com/logos/nba/${team.teamId}/global/D/logo.svg`}
+                  alt={team.teamSitesOnly.teamNickname}
+                  objectFit="cover"
+                />
+                <Link href={`/team/${team.teamSitesOnly.teamCode}`} passHref>
+                  <div className="navbar-brand linkFlex">
+                    <span className={styles.teamLink}>
+                      {team.teamSitesOnly.teamName +
+                        " " +
+                        team.teamSitesOnly.teamNickname}
+                    </span>
+                  </div>
+                </Link>
+                <span>{team.win + "-" + team.loss}</span>
               </div>
             );
           })}
         </div>
         <div className={styles.standingsConference}>
-          <h3>West</h3>
+          <div className={styles.conferenceTitle}>
+            <h3>West</h3>
+          </div>
           {west.map((team) => {
             return (
-              <div key={team.teamId} className={
-                parseInt(team.confRank) <= 8 ?
-                styles.standingsTeam + " " + styles.playoffs:
-                styles.standingsTeam
-                }>
+              <div
+                key={team.teamId}
+                className={
+                  parseInt(team.confRank) <= 8
+                    ? styles.standingsTeam + " " + styles.playoffs
+                    : styles.standingsTeam
+                }
+              >
                 <span>{team.confRank}</span>
-                <span>
                 <Image
-                    width={40}
-                    height={40}
-                    src={`https://cdn.nba.com/logos/nba/${team.teamId}/global/D/logo.svg`}
-                    alt={team.teamSitesOnly.teamNickname}
-                    objectFit="cover"
-                  />
-                  {team.teamSitesOnly.teamName +
-                    " " +
-                    team.teamSitesOnly.teamNickname}
-                </span>
-                <span>
-                  {team.win +
-                    "-" +
-                    team.loss}
-                </span>
+                  width={40}
+                  height={40}
+                  src={`https://cdn.nba.com/logos/nba/${team.teamId}/global/D/logo.svg`}
+                  alt={team.teamSitesOnly.teamNickname}
+                  objectFit="cover"
+                />
+                <Link href={`/team/${team.teamSitesOnly.teamCode}`} passHref>
+                  <div className="navbar-brand linkFlex">
+                    <span className={styles.teamLink}>
+                      {team.teamSitesOnly.teamName +
+                        " " +
+                        team.teamSitesOnly.teamNickname}
+                    </span>
+                  </div>
+                </Link>
+                <span>{team.win + "-" + team.loss}</span>
               </div>
             );
           })}
         </div>
-      </div> 
-
-    </div>  
+      </div>
+    </div>
   );
 }
